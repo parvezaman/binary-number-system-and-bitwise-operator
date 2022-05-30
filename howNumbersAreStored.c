814928935f8ab75f75154704d5fc6355353655ba
parvezaman <emailtoamanullah@gmail.com>
2022-05-30 13:28:41 +0600

    how numbers are stored

---
+++ howNumbersAreStored.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+    return 0;
+}
+
+/*
+we know, 8 bit = 1 byte
+
+take 12 as an example
+if 12 is a charecter
+12 -> 1 byte -> 8 bit -> 00001010
+
+if 12 is an integer
+12 -> 4 byte -> 32 bit -> 00000000 00000000 00000000 00001010
+*/
