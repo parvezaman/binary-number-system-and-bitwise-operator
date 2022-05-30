99170c30ef5fa189d4f96fff463063fdf4d318d8
parvezaman <emailtoamanullah@gmail.com>
2022-05-30 09:49:29 +0600

    another way to calculate binary to decimal

--- baseConversion.c
+++ baseConversion.c
@@ -2,7 +2,22 @@
 #include <string.h>
 
 // a funcation to convert binary to decimal
+// way 2 to convert binary to decimal
 unsigned int binaryToInt(char str[])
+{
+    int len = strlen(str);
+    unsigned int answer = 0;
+    for (int i = 0; i < len; i++)
+    {
+        int bit = str[i] - '0';
+        printf("%d %d\n", i, bit);
+
+        answer = answer * 2 + bit;
+    }
+    return answer;
+}
+// way 1 to convert binary to decimal
+/* unsigned int binaryToInt(char str[])
 {
     int len = strlen(str);
     unsigned int answer = 0;
@@ -16,7 +31,7 @@ unsigned int binaryToInt(char str[])
         }
     }
     return answer;
-}
+} */
 int main()
 {
     char str[100];
